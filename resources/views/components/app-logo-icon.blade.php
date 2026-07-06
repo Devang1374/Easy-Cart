ea2b3ba976efd360765c100acc5384e0e510b95c
devang1374 <vagheladevang123@gmail.com>
2026-07-06 16:57:19 +0530

    again fixing logo problam

--- resources/views/components/app-logo-icon.blade.php
+++ resources/views/components/app-logo-icon.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ Vite::asset('resources/images/easy-cart-logo.png') }}" class="bg-white dark:bg-black" alt="My Image">
+<img src="https://res.cloudinary.com/iowr4dh0/image/upload/v1783336799/easy-cart-logo_ygytxm.png" class="bg-white dark:bg-black" alt="My Image">
