c7620b50a94770e9cfe78fe10452eb47607ca31d
devang1374 <vagheladevang123@gmail.com>
2026-07-06 18:57:00 +0530

    fixing bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies();    
+        //$middleware->trustProxies();    
 
         $middleware->validateCsrfTokens(except: [
             'livewire/*',
