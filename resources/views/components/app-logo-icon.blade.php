758a4a089a7d279748ca1b3fdebdb4c05e9ecd9f
devang1374 <vagheladevang123@gmail.com>
2026-07-07 16:22:05 +0530

    backup

--- resources/views/components/app-logo-icon.blade.php
+++ resources/views/components/app-logo-icon.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ Vite::asset('resources/images/easy-cart-logo.png') }}" class="bg-white dark:bg-black" alt="My Image">
+<img src="https://res.cloudinary.com/iowr4dh0/image/upload/v1783421188/easy-cart-logo_phefi2.png" class="bg-white dark:bg-black" alt="My Image">
